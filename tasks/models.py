bfb79b2a822abca3ab2501db600f2e452e1b7e8b
Carlo <carlosaprado@gmail.com>
2023-09-09 14:05:12 -0300

    created superuser | add objects

--- tasks/models.py
+++ tasks/models.py
@@ -3,6 +3,12 @@ from django.db import models
 class TodoList(models.Model):
     list_name = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.list_name
+
 class TodoItem(models.Model):
     item_name = models.CharField(max_length=250)
     related_list = models.ForeignKey(TodoList, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.item_name
