9f10b82b044004c13d88d4c159e223dfcd5779ec
Carlo <carlosaprado@gmail.com>
2023-09-23 13:09:07 -0300

    views and urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="index"),                          # /tasks/
+    path("<int:todolist_id>/", views.todolist, name="todolist"),  # /tasks/23/
+    path("<int:item_id>/items/", views.listitems, name="listitems") # /tasks/23/items/
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,3 +2,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world! This is the tasks view.")
+
+def todolist(request, todolist_id):
+    response = "This is the todo list {}".format(todolist_id)
+    return HttpResponse(response)
+
+def listitems(request, item_id):
+    response = "This are the items from the todo list {}".format(item_id)
+    return HttpResponse(response)
