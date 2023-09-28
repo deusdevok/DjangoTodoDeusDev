3b2f8dd90db5e7ce51766e54e6d66d542bf63762
Carlo <carlosaprado@gmail.com>
2023-09-28 11:37:23 -0300

    views and urls

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,19 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import TodoList
 
 def index(request):
-    return HttpResponse("Hello, world! This is the tasks view.")
+    list_objects = TodoList.objects.all()
+    context = {
+        "list_objects": list_objects,
+    }
+
+    return render(request, "tasks/index.html", context)
 
 def todolist(request, todolist_id):
     response = "This is the todo list {}".format(todolist_id)
     return HttpResponse(response)
 
-def listitems(request, item_id):
-    response = "This are the items from the todo list {}".format(item_id)
+def listitems(request, todolist_id):
+    response = "This are the items from the todo list {}".format(todolist_id)
     return HttpResponse(response)
